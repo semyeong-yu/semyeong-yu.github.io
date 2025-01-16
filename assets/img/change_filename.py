0d5456409bc435465ef98acdf9416776f2242919
jaehyun <kjaehyun04@gmail.com>
2025-01-16 15:51:46 +0900

    20250116_modifyimgname

--- assets/img/change_filename.py
+++ assets/img/change_filename.py
@@ -5,11 +5,12 @@ def change_imgname(root_dir):
     dirs = os.listdir(root_dir)
     for dirr in dirs:
         dir_path = os.path.join(root_dir, dirr)
-        files = os.listdir(dir_path)
-        for file in files:
-            if file.endswith('.png') or file.endswith('.PNG') or file.endswith('.jpg') or file.endswith('.JPG'):
-                new_file = file.rsplit('.', 1)[0] + 'm.PNG'
-                os.rename(os.path.join(dir_path, file), os.path.join(dir_path, new_file))
+        if os.path.isdir(dir_path):
+            files = os.listdir(dir_path)
+            for file in files:
+                if file.endswith('.png') or file.endswith('.PNG') or file.endswith('.jpg') or file.endswith('.JPG'):
+                    new_file = file.rsplit('.', 1)[0] + 'm.PNG'
+                    os.rename(os.path.join(dir_path, file), os.path.join(dir_path, new_file))
 
 if __name__ == "__main__":
     change_imgname("./")
